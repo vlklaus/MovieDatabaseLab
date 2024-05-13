842426c6be7522376608f7695d5112fd3e8f77f8
Victoria Klaus <vlklaus22@gmail.com>
2024-05-13 16:52:34 -0400

    first commit

---
+++ 051324 Movie Database Lab/Movie.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _051324_Movie_Database_Lab
+{
+    public class Movie
+    {
+        // properties
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public static object TryParse { get; private set; }
+
+
+        // constructor
+        public Movie(string _title, string _category)
+        {
+            Title = _title;
+            Category = _category;
+        }
+
+
+        // continue program method loop
+        public static bool GetContinue()
+        {
+            bool result = true;
+            while (true)
+            {
+                Console.Write("See a new category? (y/n) ");
+                string choice = Console.ReadLine().ToLower().Trim();
+
+                if (choice == "y")
+                {
+                    result = true;
+                    break;
+                } else if (choice == "n")
+                {
+                    result = false;
+                    break;
+                } else
+                {
+                    Console.WriteLine("Invalid input. Try again.");
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ 051324 Movie Database Lab/Program.cs
@@ -0,0 +1,51 @@
+﻿using _051324_Movie_Database_Lab;
+
+List<Movie> movies = new List<Movie>()
+{
+    new Movie("Interstellar", "scifi"), //movies.Add(new Movie ("Interstellar", "Sci-Fi")); // Sci-Fi, Adventure
+    new Movie("Eternal Sunshine of the Spotless Mind", "romance"),
+    new Movie("Barbarian", "horror"),
+    new Movie("Talk to Me", "horror"),
+    new Movie("How to Lose a Guy in 10 Days", "romance"),
+    new Movie("Back to the Future", "scifi"),
+    new Movie("Jujutsu Kaisen 0", "action"),
+    new Movie("Colorful", "fantasy"),
+    new Movie("Fight Club", "thriller"),
+    new Movie("Goodwill Hunting", "drama"),
+    new Movie("Killers of the Flower Moon", "crime"),
+    new Movie("Goodfellas", "crime")
+};
+
+Console.WriteLine("Welcome to the Movie List Application!");
+Console.WriteLine($"There are {movies.Count} movies in this list.");
+
+bool runAgain = true;
+while (runAgain)
+{       
+    Console.Write("What category are you interest in? ");
+    string category = Console.ReadLine().ToLower().Trim();
+
+    // category not available 
+    if (category != "scifi" && category != "romance" && category != "horror" && category != "action" && category != "action" && category != "thriller" && category != "drama" && category != "crime")
+    {
+        Console.WriteLine("We don't have any movies with that category. Try again please!");
+        continue;
+    }
+           
+    foreach (Movie movie in movies) // finding the correct category
+    {
+        if (movie.Category == category)
+        {
+            List<Movie> moviefiltered = movies.Where(m => m.Category == category).ToList();
+            foreach (Movie m in moviefiltered)
+            {
+
+                Console.WriteLine(m.Title);
+            }
+            break;
+        }
+    }
+
+    Console.WriteLine();
+    runAgain = Movie.GetContinue(); // loop program again
+}
